de6afbf0a26bfcc3f399d43e14ae3de99552d4ec
johngarcia9110 <johngarcia9110@gmail.com>
2017-02-15 19:08:30 -0500

    added semicolons

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
-const app = express()
+const app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
 //process data
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //routes
 app.get('/', function(req, res){
-  res.send("Hi I am a chatbot")
-})
+  res.send("Hi I am a chatbot");
+});
 
 //Facebook
 app.get('/webhook/', function(req,res){
   if(req.query['hub.verify_token'] === 'johnbgarcia'){
-    res.send(req.query['hub.challenge'])
+    res.send(req.query['hub.challenge']);
   }
-  res.send('Wrong Token')
-})
+  res.send('Wrong Token');
+});
 
 //server
 app.listen(app.get('port'), function(){
-  console.log('Running on: port.')
-})
+  console.log('Running on: port.');
+});
